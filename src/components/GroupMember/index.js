1489e8071b2ef0a6d1f2d7781048cc38e995a34a
TrAyZeN <1810leo@gmail.com>
2020-04-02 18:47:28 +0200

    Rewrite to functional component and update alt

--- src/components/GroupMember/index.js
+++ src/components/GroupMember/index.js
@@ -5,40 +5,34 @@ import github from '../../assets/github.svg';
 import gmail from '../../assets/gmail.svg'
 import linkedin from '../../assets/linkedin.svg';
 
-class GroupMember extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function GroupMember(props) {
+  return (
+    <div className="GroupMember">
+      <img src={props.picture}
+            alt="Member" />
 
-  render() {
-    return (
-      <div className="GroupMember">
-        <img src={this.props.picture}
-             alt="member-picture" />
+      <div className="GroupMember-information">
+        <h2>{props.name}</h2>
 
-        <div className="GroupMember-information">
-          <h2>{this.props.name}</h2>
+        <div className="GroupMember-links">
+          <a href={props.github}>
+            <img src={github}
+                  alt="GitHub icon" />
+          </a>
 
-          <div className="GroupMember-links">
-            <a href={this.props.github}>
-              <img src={github}
-                   alt="github-icon" />
-            </a>
+          <a href={"mailto:" + props.mail}>
+            <img src={gmail}
+                  alt="Gmail icon" />
+          </a>
 
-            <a href={"mailto:" + this.props.mail}>
-              <img src={gmail}
-                   alt="gmail-icon" />
-            </a>
-
-            <a href={this.props.linkedin}>
-              <img src={linkedin}
-                   alt="linkedin-icon" />
-            </a>
-          </div>
+          <a href={props.linkedin}>
+            <img src={linkedin}
+                  alt="LinkedIn icon" />
+          </a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default GroupMember;
