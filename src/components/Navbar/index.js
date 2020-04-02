fb2d616bbda24f4d1334b1e7bf54aec880d05f79
TrAyZeN <1810leo@gmail.com>
2020-04-02 18:37:42 +0200

    Rewrite Navbar to functional component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './style.scss';
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <div className="Navbar">
-        <a href="/#presentation">Présentation</a>
-        <a href="/#groupe">Groupe</a>
-        <a href="/#telechargements">Téléchargements</a>
-        <a href="/#contacts">Nous contacter</a>
-        <a href="/documentation">Documentation</a>
-      </div>
-    );
-  }
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <a href="/#presentation">Présentation</a>
+      <a href="/#groupe">Groupe</a>
+      <a href="/#telechargements">Téléchargements</a>
+      <a href="/#contacts">Nous contacter</a>
+      <a href="/documentation">Documentation</a>
+    </div>
+  );
 }
 
 export default Navbar;
