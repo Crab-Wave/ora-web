041bd1b1caa607c7038664b217bfc54bd3622f30
TrAyZeN <1810leo@gmail.com>
2020-04-03 12:30:01 +0200

    Add sidebar and classname

--- src/pages/Documentation/index.js
+++ src/pages/Documentation/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import DocumentationSidebar from '../../components/DocumentationSidebar';
+import './style.scss';
+
+import OraDoc from '../../documentation.json';
 
 class Documentation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      "className": this.props.match.params.className,
+    };
+  }
+
   render() {
     return (
       <div className="Documentation">
         <Navbar />
+        <DocumentationSidebar />
+
+        <div className="Documentation-class">
+          <h1>{this.state.className}</h1>
+          <p>{OraDoc[this.state.className].summary}</p>
+        </div>
       </div>
     );
   }
