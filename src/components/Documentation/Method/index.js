bec046365403f23e8ee92ab71f2d8fbe2f66813f
TrAyZeN <1810leo@gmail.com>
2020-04-05 13:44:36 +0200

    Add Method component

---
+++ src/components/Documentation/Method/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+function Method(props) {
+  return (
+    <div className="Method">
+      <p>{props.method}</p>
+    </div>
+  );
+}
+
+export default Method;
