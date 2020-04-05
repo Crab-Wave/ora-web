a2d500773dd4237e92471f5b8d90ba52ef5ab00c
TrAyZeN <1810leo@gmail.com>
2020-04-05 13:45:48 +0200

    Update import and add example

--- src/pages/Documentation/index.js
+++ src/pages/Documentation/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
-import DocumentationSidebar from '../../components/DocumentationSidebar';
+import Sidebar from '../../components/Documentation/Sidebar';
+import Method from '../../components/Documentation/Method';
 import './style.scss';
 
 import OraDoc from '../../documentation.json';
@@ -18,11 +19,30 @@ class Documentation extends React.Component {
     return (
       <div className="Documentation">
         <Navbar />
-        <DocumentationSidebar />
+        <Sidebar />
 
-        <div className="Documentation-class">
+        <div className="Documentation-body">
           <h1>{this.state.className}</h1>
           <p>{OraDoc[this.state.className].summary}</p>
+
+          <div className="Documentation-example">
+            <h1>Examples</h1>
+
+            <p>{OraDoc[this.state.className].example.text}</p>
+            <pre>
+              <code>
+                {OraDoc[this.state.className].example.code}
+              </code>
+            </pre>
+          </div>
+
+          <div className="Documentation-methods">
+            <h1>Methods</h1>
+
+            {OraDoc[this.state.className].methods.map(
+              m => <Method method={m.prototype} />
+            )}
+          </div>
         </div>
       </div>
     );
