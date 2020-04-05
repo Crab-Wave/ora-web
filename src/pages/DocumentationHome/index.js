5242c3c1a2ab946ec61a34d2df140d29ad42820b
TrAyZeN <1810leo@gmail.com>
2020-04-05 13:45:09 +0200

    Update import and add padding

--- src/pages/DocumentationHome/index.js
+++ src/pages/DocumentationHome/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
-import DocumentationSidebar from '../../components/DocumentationSidebar';
+import Sidebar from '../../components/Documentation/Sidebar';
 import './style.scss';
 
 function DocumentationHome() {
   return (
     <div className="DocumentationHome">
       <Navbar />
-      <DocumentationSidebar />
+      <Sidebar />
 
       <div className="DocumentationHome-body">
         <h1>Welcome to ORA documentation pages</h1>
