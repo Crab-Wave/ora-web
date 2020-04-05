760dcd6dd4c478833621c158b4275eeea91b3a82
TrAyZeN <1810leo@gmail.com>
2020-04-05 13:44:13 +0200

    Move and rename DocumentationSidebar

---
+++ src/components/Documentation/Sidebar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.scss';
+
+import OraDoc from '../../../documentation.json'
+
+function Sidebar() {
+  return (
+    <div className="DocumentationSidebar">
+      <Link to="/documentation">Welcome page</Link>
+
+      {Object.keys(OraDoc).map(className =>
+        <Link to={`/documentation/${className}`}>{className}</Link>
+      )}
+    </div>
+  );
+}
+
+export default Sidebar;
