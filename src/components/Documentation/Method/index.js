33f896491194870080e6c903662c637434578391
TrAyZeN <1810leo@gmail.com>
2020-04-15 12:29:32 +0200

    Add elements to component

--- src/components/Documentation/Method/index.js
+++ src/components/Documentation/Method/index.js
@@ -4,7 +4,12 @@ import './style.scss';
 function Method(props) {
   return (
     <div className="Method">
-      <p>{props.method}</p>
+      <h2>{props.method.prototype}</h2>
+      <p>{props.method.summary}</p>
+      <h3>Parameters</h3>
+      <p>{props.method.param.text}</p>
+      <h3>Returns</h3>
+      <p>{props.method.returns}</p>
     </div>
   );
 }
