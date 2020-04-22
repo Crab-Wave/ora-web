0ec2f6c3bd6831558b06ef7656f75ebfa7b719dc
TrAyZeN <1810leo@gmail.com>
2020-04-22 22:15:17 +0200

    Don't add return section if there is no return

--- src/components/Documentation/Method/index.js
+++ src/components/Documentation/Method/index.js
@@ -8,8 +8,13 @@ function Method(props) {
       <p>{props.method.summary}</p>
       <h3>Parameters</h3>
       <p>{props.method.param.text}</p>
-      <h3>Returns</h3>
-      <p>{props.method.returns}</p>
+      {
+        !props.method.hasOwnProperty("returns") ? null :
+        <div>
+          <h3>Returns</h3>
+          <p>{props.method.returns}</p>
+        </div>
+      }
     </div>
   );
 }
